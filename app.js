48c118730b9d23994bbffcee9dfd77dee8d6d5c1
cixil <cecilia@airmail.cc>
2017-12-14 19:04:39 -0500

    Got rid of ember and database

--- app.js
+++ app.js
@@ -1,48 +1,23 @@
-import Application from '@ember/application';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
-
-const App = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
-
-// Content above is ember specific shit, don't change.
-
-// CTRL-F + ? to find comments marking issues
-// CTRL-F + ^^ to find comments marking possible additional features if time allows
-
-/*		For Programming Reference (class/Ids used) :
-		- Settings -
-		.short-break
-		.long-break
-		.study-time
-
-		- To Do -
-		#todo
-		#task-list	-append todo entries here
-*/
-
 $(document).ready( function(){
-
 		var shortBreak = 5, longBreak = 25, studyTime = 25;
 		var interval = studyTime;
 		var isStudyTime = true;
 		var pomsperset = 4;
 		var poms = 0;
+		var completedPomImage = "assets/images/favicon.png";
 
-		var alarmPath = "/assets/alarms/"; // path to alarm sounds
+		var alarmPath = "assets/alarms/"; // path to alarm sounds
 		var alarms = [
 				{name:"Store Chime", path:(alarmPath + "store-chime.mp3")},
 				{name:"Metronome", path:(alarmPath + "metronome.mp3")},
 				{name:"Ship Bell", path:(alarmPath + "ship-bell.mp3")},
-				{name:"Zen Temple", path:(alarmPath + "zen-temple.mp3")}
+				{name:"Zen Temple", path:(alarmPath + "zen-temple.mp3")},
+				{name:"Buzzer", path:(alarmPath + "buzzer.mp3")},
+				{name:"EAS beep", path:(alarmPath + "eas-beep.mp3")}
 				//{name:"", path:(alarmPath + "")},
 		];
 		var alarmIndex = 0; // default alarm
-
+		//var repeat = toggleRepeat();
 		var paused = false;
 		
 		var username;
@@ -57,10 +32,16 @@ $(document).ready( function(){
 		$('.alarm-menu').change(function(){ playAlarm(); });
 
 		function playAlarm(){
-				console.log("index= "+document.getElementById('sel1').selectedIndex);
 				alarmIndex = document.getElementById('sel1').selectedIndex;
 				$('#audio').html('<audio id=\"audio'+alarmIndex+'\" source src=\"'+alarms[alarmIndex].path+'\" type=\"audio/mpeg\"></audio>');	
 				document.getElementById("audio"+alarmIndex).play();
+		}
+
+		function toggleRepeat(){
+				if ($('#repeat').prop('checked')){
+						repeat = true;
+				}
+				else { repeat = false; }
 		}
 
 
@@ -69,15 +50,15 @@ $(document).ready( function(){
 		function setCookie( cname, cvalue, exdays) {
 				var d = new Date();
 				d.setTime(d.getTime() + (exdays*24*60*60*1000));
-				document.cookie = cname + "=" + cvalue + "; expires="+d.toUTCString() + ";path=/";
+				document.cookie = cname + "=" + cvalue + ", expires="+ d.toUTCString() + ", path=/;";
+				
+				console.log("cookie set: " + document.cookie);
 		}
-
-		//if cookie expired call deleteUSER
 
 		function getCookie(cname) {
 				var name = cname + "=";
 				var decodedCookie = decodeURIComponent(document.cookie);
-				var ca = decodedCookie.split(';');
+				var ca = decodedCookie.split(',');
 				for(var i = 0; i <ca.length; i++) {
 						var c = ca[i];
 						while (c.charAt(0) == ' ') {
@@ -92,75 +73,60 @@ $(document).ready( function(){
 
 		function getNewUser(){
 				var newuser= "";
-				while (newuser =="" | newuser == null | userInDB()) {
-						if (!userIsValid(newuser)){ 
-								newuser = prompt("That username is taken, please choose another: ", "");
-						} else {
-								newuser = prompt("Please enter a unique username: ", "");
-						}
+				while (newuser =="" | newuser == null ) {
+					newuser = prompt("Please enter a unique username: ", "");
 				}
-				addUser(user);
 				return newuser;
 		}
 
 		function checkCookie() {
 				var user = getCookie("user");
 				if (user != "") { //cookie exists
+						console.log("cookie exists");
 						username = user;
-
 				} else {
-						user = getNewUser();
-						setCookie("user", user, expiration);
+						username = getNewUser();
+						setCookie("user", username, expiration);
 				}
 		}
 
 		checkCookie();
-		saveSettings();
-
-
-		// DataBase Functions / Set User Settings  *****************************************************
+		restore();
 		
-		function userIsValid(user){  
-				// return true if user not in DB or if user is expired, false if not <- Molly?
-				return true;
-		}
-
-		function addUser(user) {
-				// add user and a time stamp to DB
-		}
-
-		function saveSettings(user) {
-				// save shortbreak, long, etc to DB
-
-				//leave this for now
-				shortBreak = $('.short-break').val();
-				longBreak = $('.long-break').val();
-				studyTime = $('.study-time').val();
-				pomsperset = $('.pomsperset').val();
-				alarmIndex = document.getElementById('sel1').selectedIndex; // fix this if we add another sel1
-		}
-
-		function restore(user) {
-				// set whatever settings (short break, long break, etc) from DB and display should update automatically
-				// display todo list
-		}
-
 		$('#update-settings').click(function() {
 				var txt, confmsg = "Your current progress and timer will be reset, are you sure?\n To Do list will be saved.";
 				if (confirm(confmsg) == true)
 				{
 						$('#pause').text("Pause");
-						saveSettings();
-						poms=0;
+						toggleRepeat();
+						restore();
+						resetPoms();
 						restartTimer();
 				}
 		});
-		
-		
-		// Change The Display Functions ****************************************************************
 
-		$('#pause').text("Start");
+		function restore() {
+				var sb = $('.short-break').val();
+				var lb = $('.long-break').val();
+				var st = $('.study-time').val();
+				var poms = $('.pomsperset').val();
+				var alarm = document.getElementById('sel1').selectedIndex; 
+		}
 		
+
+
+		// Setup The Display ****************************************************************
+		$('#pause').text("Start");
+		$('#title').text(username +"'s Pomodoro");  // Personalize title with username from cookie
+		$('#timer').text(add0(studyTime)+":00"); 			// set initial time display.
+	
+		// Communicate with User - used to display message after intervals of study or break 
+		function say(message){
+				alert(message) //going to change later?
+		}
+
+
+		// Pomodoro Buttons ****************************************************************************
 		$('#pause').click(function(){
 				switch($('#pause').text()) {
 						case "Start":
@@ -176,14 +142,14 @@ $(document).ready( function(){
 								resumeTimer();
 				}
 		});
-		
-		$('#title').text(username +"'s Pomodoro");  // Personalize title with username from cookie
-		$('#timer').text(studyTime+":00"); 			// set initial time display.
 
-		// Communicate with User - used to display message after intervals of study or break 
-		function say(message){
-				alert(message) //going to change later?
-		}
+		$('#reset').click(function(){
+				var confmsg = "Reset the current interval?\nYou won't lose your completed pomodoro count.";
+				if (confirm(confmsg)){
+						startTimer(interval, 0);
+				}
+		});
+		
 
 
 		// Pomodoro technique algorithm to ensure the proper interval for the timer *********************
@@ -199,7 +165,7 @@ $(document).ready( function(){
 								interval = studyTime;
 								break;
 						case longBreak:
-								poms = 0;	//reset poms
+								resetPoms();
 				}
 				if (poms == 0){
 						interval = studyTime;
@@ -212,6 +178,29 @@ $(document).ready( function(){
 				return (poms % pomsperset == 0);
 		}
 
+		function pomCompleted(){
+				if (poms == pomsperset) { 
+						resetPoms();
+				}
+				else {
+						poms++;
+						$('#pom-tally').append('<img class=\"pom-tally-image\" src=\"'+completedPomImage+'\" alt=\"|\">');
+				}
+				
+		}
+
+		function resetPoms(){
+				poms = 0;
+				$('#pom-tally').html(" ");
+		}
+		$('.delete-poms').click(function(){
+				if(confirm("Reset current progress? This will delete your completed pomodoro history and restart the timer.")){
+						resetPoms();
+						interval = studyTime;
+						startTimer(interval, 0);
+				}
+		});
+				
 
 		// Timer *****************************************************************************************
 
@@ -236,7 +225,7 @@ $(document).ready( function(){
 				milliseconds = distance % second,
 				countdownElement = document.getElementById('timer');
 
-				countdownElement.textContent = minutes + ':' + seconds;
+				countdownElement.textContent = add0(minutes) + ':' + add0(seconds);
 		}
 
 		function timerEnd() { // What happens when interval has ended
@@ -245,11 +234,10 @@ $(document).ready( function(){
 				switch (interval){
 						case studyTime: 
 								if (isStudyTime){ 	// Added extra check in case studyTime and longBreak are the same amount.
-										poms++;		// VERY IMPORTANT
-										if (isTimeForLongBreak) { say("Nice work. Take a break!");}	// add a feature to pull a message randomly from a list^^ ie: "Time for a snack!", "Nice work!", "Let's get shit done"
+										pomCompleted();		// VERY IMPORTANT
+										if (isTimeForLongBreak) { say("Nice work. Take a break!");}	
 								}
-								//else { continue; }	// should skip the break and jump to case long break.
-								break;				// not sure if works yet.
+								break;				
 						case longBreak:
 								say("Time to get back to work!");
 								break;
@@ -259,8 +247,7 @@ $(document).ready( function(){
 		}
 
 		function startTimer(m, s) {
-				console.log("in start timer func. Interval: "+ interval);
-				console.log("poms: "+poms);
+				$('#pause').text("Pause");
 				end = Date.now() + (m * 60 * 1000) + (s*1000);
 				id = setInterval(showTimer, 10);
 		}
@@ -275,20 +262,15 @@ $(document).ready( function(){
 				startTimer( parseInt(t[0], 10), parseInt(t[1], 10));
 		}
 
+		function add0(n){
+				if (n < 10) {
+						return "0"+n;
+				}
+				return n;
+		}
+
 		function restartTimer(){
 				pauseTimer();
 				startTimer(interval = getInterval(), 0);
 		}
-
-
-		// *****************************************************************************************
-		// Add code to append to do list items and strike them out when clicked?
-		// Keep in a database and associate with a session cookie?
-		
-		// Add way to keep track of completed poms?
-
 });
-
-loadInitializers(App, config.modulePrefix);
-
-export default App;
